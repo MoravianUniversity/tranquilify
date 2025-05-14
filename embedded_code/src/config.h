cf32ab2c00444c9bc23000246382aa5767dceff1
Jeffrey Bush <jeff@coderforlife.com>
2025-05-14 10:12:37 -0400

    Adding general config header

---
+++ embedded_code/src/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// General config for the project
+
+// If defined, the program will print debug messages to the serial port and perform other debugging tasks
+#define DEBUG 1
+
+// The pin that the debug LED is connected to
+#define DEBUG_LED_PIN 13
