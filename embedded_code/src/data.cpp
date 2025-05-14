46d2db1237d9f5af583177fe964dd789d0928dfa
Jeffrey Bush <jeff@coderforlife.com>
2025-05-14 13:18:53 -0400

    Fixing some issues with data file writing.

--- embedded_code/src/data.cpp
+++ embedded_code/src/data.cpp
@@ -20,13 +20,13 @@ bool nextFiles(SdFs* sd) {
     int counter = 0;
 
     sprintf(audioFileName, "/audio_%06d.wav", counter);
-    FsFile audioFile = sd->open(audioFileName, O_WRONLY | O_CREAT | O_EXCL);
+    FsFile audioFile = sd->open(audioFileName, O_WRONLY | O_CREAT);  // TODO: O_EXCL
     if (!audioFile) { Serial.printf("!! Failed to create file '%s'\n", audioFileName); return false; }
     if (!startWAVFile(audioFile)) { return false; }
     audioFile.close();
     
     sprintf(timestampFileName, "/timestamps_%06d.txt", counter);
-    FsFile timestampFile = sd->open(timestampFileName, O_WRONLY | O_CREAT | O_EXCL);
+    FsFile timestampFile = sd->open(timestampFileName, O_WRONLY | O_CREAT);  // TODO: O_EXCL
     if (!timestampFile) { Serial.printf("!! Failed to create file '%s'\n", timestampFileName); return false; }
     startTimestamp = millis(); // the time that the timestamps are relative to
     timestampFile.close();
@@ -54,7 +54,8 @@ bool ensureFiles(SdFs* sd) {
 bool writeWAVData(SdFs* sd, WriteWAVParams *params) {
     if (!ensureFiles(sd)) { return false; }
 
-    FsFile file = sd->open(audioFileName, O_WRONLY | O_APPEND | O_EXCL);
+    FsFile file = sd->open(audioFileName, O_WRONLY);
+    if (!file) { Serial.printf("!! Failed to open file '%s'\n", audioFileName); return false; }
     size_t data_size = appendWAVData(file, params->buffer, params->length);
     file.close();
     params->writing = false;
@@ -68,12 +69,12 @@ bool writeButtonData(SdFs* sd, ButtonEvent* event) {
 
     // Note: technically this could end up writing negative numbers, but then they apply to the previous file
     // TODO: worse than that, we need the timestamp to be relative to the start of the recording, not the start of the files being open (which could be 100ms off!)
-    FsFile file = sd->open(timestampFileName, O_WRONLY | O_APPEND | O_EXCL);
+    FsFile file = sd->open(timestampFileName, O_WRONLY | O_APPEND);
+    if (!file) { Serial.printf("!! Failed to open file '%s'\n", timestampFileName); return false; }
     bool error = file.printf("%ld %ld\n", event->pressTime - startTimestamp, event->releaseTime - startTimestamp) == 0;
-    file.close();
     if (error) {
-        Serial.println("!! Error writing button data to file");
-        return false;
+        Serial.printf("!! Error writing button data to file (%ld %ld)\n", file.getWriteError());
     }
-    return true;
+    file.close();
+    return !error;
 }
